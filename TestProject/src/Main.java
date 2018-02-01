82ccd72f345214419c74e4b9c459726701149588
TheFlyingPenguin <bpay0490@students.kleinisd.net>
2018-02-01 09:01:05 -0600

    Commit 2

--- TestProject/src/Main.java
+++ TestProject/src/Main.java
@@ -2,8 +2,12 @@ public class Main {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		//1. make changes
-		//2. Commit
-		//3. Push to GitHub repository
+		//Order of Operations(GitHub):
+			//1. Make changes
+				// A. Run program to save your personal version
+			//2. Commit
+				// A. Saves to our local repository
+			//3. Push to GitHub repository
+				// A. Sends to the GitHub cloud
 	}
 }
